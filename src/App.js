6f09fa8068d3cfdbe8b41790f93495e7cd376eae
sonnasdu <77963072+sonnasdu@users.noreply.github.com>
2021-08-10 00:22:08 +0700

    Update App.js
    Update Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         SONNA FREE REACTJS APP FREE HOSTING
         </p>
         <a
           className="App-link"
